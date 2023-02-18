212162fe320725d20e583abae49ccc340f453ded
SULTANOV JASUR <99758188+JAJU05@users.noreply.github.com>
2023-02-19 02:36:26 +0500

    Models

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+#
+# from home.views import
+#
+# router = DefaultRouter()
+# router.register('massage', MassageModelViewSet, 'massage')
+#
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
